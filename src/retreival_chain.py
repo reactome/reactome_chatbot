c7fd1e56e937e2c4ebaa8faacf89009d27b72200
Helia Mohammadi <helia.mohammadi01@gmail.com>
2024-11-22 14:57:32 -0500

    Added BM25 retriever

--- src/retreival_chain.py
+++ src/retreival_chain.py
@@ -5,8 +5,11 @@ from typing import Callable, Optional
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.retrievers import EnsembleRetriever
+from langchain.retrievers.merger_retriever import MergerRetriever
 from langchain.retrievers.self_query.base import SelfQueryRetriever
 from langchain_chroma.vectorstores import Chroma
+from langchain_community.document_loaders.csv_loader import CSVLoader
+from langchain_community.retrievers import BM25Retriever
 from langchain_core.embeddings import Embeddings
 from langchain_core.retrievers import RetrieverLike
 from langchain_huggingface import (HuggingFaceEmbeddings,
@@ -17,6 +20,7 @@ from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from conversational_chain.graph import RAGGraphWithMemory
 from conversational_chain.memory import ChatHistoryMemory
 from reactome.metadata_info import descriptions_info, field_info
+from util.embedding_environment import EmbeddingEnvironment
 
 
 def list_chroma_subdirectories(directory: Path) -> list[str]:
@@ -85,25 +89,44 @@ def initialize_retrieval_chain(
     # Get OpenAIEmbeddings (or HuggingFaceEmbeddings model if specified)
     embedding_callable = get_embedding(hf_model, device)
 
+    csv_file_mapping = {
+        "complexes": "complexes.csv",
+        "reactions": "reactions.csv",
+        "summations": "summations.csv",
+        "ewas": "ewas.csv",
+    }
+
     # Adjusted type for retriever_list
     retriever_list: list[RetrieverLike] = []
     for subdirectory in list_chroma_subdirectories(embeddings_directory):
+        # set up BM25 retriever
+        csv_file_name = csv_file_mapping[subdirectory]
+        reactome_csvs_dir: Path = EmbeddingEnvironment.get_dir("reactome") / "csv_files"
+        loader = CSVLoader(file_path=str(reactome_csvs_dir) + "/" + csv_file_name)
+        data = loader.load()
+        bm25_retriever = BM25Retriever.from_documents(data)
+        bm25_retriever.k = 15
+
+        # set up vectorstore SelfQuery retriever
         embedding = embedding_callable()
         vectordb = Chroma(
             persist_directory=str(embeddings_directory / subdirectory),
             embedding_function=embedding,
         )
 
-        retriever = SelfQueryRetriever.from_llm(
+        selfq_retriever = SelfQueryRetriever.from_llm(
             llm=llm,
             vectorstore=vectordb,
             document_contents=descriptions_info[subdirectory],
             metadata_field_info=field_info[subdirectory],
-            search_kwargs={"k": 10},
+            search_kwargs={"k": 15},
         )
-        retriever_list.append(retriever)
+        rrf_retriever = EnsembleRetriever(
+            retrievers=[bm25_retriever, selfq_retriever], weights=[0.2, 0.8]
+        )
+        retriever_list.append(rrf_retriever)
 
-    reactome_retriever = EnsembleRetriever(
+    reactome_retriever = MergerRetriever(
         retrievers=retriever_list, weights=[0.25] * len(retriever_list)
     )
 
