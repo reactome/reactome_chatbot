a34e8fb4f7509acf8711a7c3a14a431de3325e7c
Helia Mohammadi <helia.mohammadi01@gmail.com>
2025-02-24 16:51:39 -0500

    integrate chainlit with the new 'cross-database' system

---
+++ bin/cross_database_chat.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+
+import os
+from typing import Any
+from pathlib import Path
+
+import chainlit as cl
+from chainlit.data.base import BaseDataLayer
+from chainlit.data.sql_alchemy import SQLAlchemyDataLayer
+from chainlit.types import ThreadDict
+from dotenv import load_dotenv
+from langchain_community.callbacks import OpenAICallbackHandler
+
+from src.conversational_chain.cross_database_graph import CrossDatabaseRAGGraph
+from src.retreival_chain import create_retriever
+from src.util.chainlit_helpers import (is_feature_enabled, message_rate_limited,
+                                   save_openai_metrics, static_messages,
+                                   update_search_results)
+from src.util.config_yml import Config, TriggerEvent
+from src.util.embedding_environment import EmbeddingEnvironment
+from src.util.logging import logging
+
+from src.reactome.metadata_info import reactome_descriptions_info, reactome_field_info
+from src.uniprot.metadata_info import uniprot_descriptions_info, uniprot_field_info
+from src.system_prompt.reactome_prompt import reactome_qa_prompt
+from src.system_prompt.uniprot_prompt import uniprot_qa_prompt
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+config: Config | None = Config.from_yaml()
+
+ENV = os.getenv("CHAT_ENV", "reactome")
+logging.info(f"Selected environment: {ENV}")
+
+reactome_retriever=create_retriever(
+        embeddings_directory=Path("/Users/hmohammadi/Desktop/react_to_me_github/reactome_chatbot/embeddings/openai/text-embedding-3-large/reactome/Release91/"), 
+        descriptions_info=reactome_descriptions_info, 
+        field_info=reactome_field_info,
+        hf_model=EmbeddingEnvironment.get_model("reactome"),
+    )
+uniprot_retriever=create_retriever(
+        embeddings_directory=Path("/Users/hmohammadi/Desktop/react_to_me_github/reactome_chatbot/src/uniprot/embeddings/3-large/2025_02/"), 
+        descriptions_info=uniprot_descriptions_info, 
+        field_info=uniprot_field_info,
+        hf_model=EmbeddingEnvironment.get_model("reactome"),
+    )
+
+llm_graph: CrossDatabaseRAGGraph = CrossDatabaseRAGGraph(
+    llm=ChatOpenAI(temperature=0.0, model="gpt-4o-mini"),
+    reactome_retriever= reactome_retriever,
+    uniprot_retriever= uniprot_retriever,
+    reactome_prompt= reactome_qa_prompt,
+    uniprot_prompt= uniprot_qa_prompt,
+    )
+
+if os.getenv("POSTGRES_CHAINLIT_DB"):
+    CHAINLIT_DB_URI = f"postgresql+psycopg://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@postgres:5432/{os.getenv('POSTGRES_CHAINLIT_DB')}?sslmode=disable"
+
+    @cl.data_layer
+    def get_data_layer() -> BaseDataLayer:
+        return SQLAlchemyDataLayer(conninfo=CHAINLIT_DB_URI)
+
+else:
+    logging.warning("POSTGRES_CHAINLIT_DB undefined; Chainlit persistence disabled.")
+
+if os.getenv("CHAINLIT_AUTH_SECRET"):
+
+    @cl.oauth_callback
+    def oauth_callback(
+        provider_id: str,
+        token: str,
+        raw_user_data: dict[str, str],
+        default_user: cl.User,
+    ) -> cl.User | None:
+        return default_user
+
+
+@cl.set_chat_profiles
+async def chat_profile() -> list[cl.ChatProfile]:
+    return [
+        cl.ChatProfile(
+            name="React-to-me",
+            markdown_description="An AI assistant specialized in exploring **Reactome** biological pathways and processes.",
+        )
+    ]
+
+
+@cl.on_chat_start
+async def start() -> None:
+    thread_id: str = cl.user_session.get("id")
+    cl.user_session.set("thread_id", thread_id)
+    await static_messages(config, TriggerEvent.on_chat_start)
+
+
+@cl.on_chat_resume
+async def resume(thread: ThreadDict) -> None:
+    await static_messages(config, TriggerEvent.on_chat_resume)
+
+
+@cl.on_chat_end
+async def end() -> None:
+    await static_messages(config, TriggerEvent.on_chat_end)
+
+
+@cl.on_message
+async def main(message: cl.Message) -> None:
+    if await message_rate_limited(config):
+        return
+
+    await static_messages(config, TriggerEvent.on_message)
+
+    message_count: int = cl.user_session.get("message_count", 0) + 1
+    cl.user_session.set("message_count", message_count)
+
+    thread_id: str = cl.user_session.get("thread_id")
+
+    chainlit_cb = cl.AsyncLangchainCallbackHandler(
+        stream_final_answer=True,
+        force_stream_final_answer=True,  # we're not using prefix tokens
+    )
+    openai_cb = OpenAICallbackHandler()
+
+    enable_postprocess: bool = is_feature_enabled(config, "postprocessing")
+
+
+    result: dict[str, Any] = await llm_graph.ainvoke(
+        message.content,
+        callbacks=[chainlit_cb, openai_cb],
+        thread_id=thread_id,
+        enable_postprocess=enable_postprocess,
+    )
+
+    if (
+        enable_postprocess
+        and chainlit_cb.final_stream
+        and len(result["additional_content"]["search_results"]) > 0
+    ):
+        await update_search_results(
+            result["additional_content"]["search_results"],
+            chainlit_cb.final_stream,
+        )
+
+    await static_messages(config, after_messages=message_count)
+
+    save_openai_metrics(message.id, openai_cb)
